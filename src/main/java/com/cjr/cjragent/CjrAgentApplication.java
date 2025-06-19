efe4afa92b135dfb5853f1efc2c075e57ee310b4
chengjinrui <sx.chengjinrui@h3c.com>
2025-06-19 23:03:12 +0800

    后端创建初始化

---
+++ src/main/java/com/cjr/cjragent/CjrAgentApplication.java
@@ -0,0 +1,13 @@
+package com.cjr.cjragent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjrAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CjrAgentApplication.class, args);
+    }
+
+}

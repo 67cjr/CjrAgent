2a6ed920da3fa14d80407874765507c994fe4666
chengjinrui <sx.chengjinrui@h3c.com>
2025-06-22 21:50:13 +0800

    多轮对话AI应用开发

---
+++ src/test/java/com/cjr/cjragent/app/LoveAppTest.java
@@ -0,0 +1,33 @@
+package com.cjr.cjragent.app;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class LoveAppTest {
+
+    @Resource
+    private LoveApp loveApp;
+
+    @Test
+    void testChat() {
+        String charId = UUID.randomUUID().toString();
+        //第一轮
+        String message = "你好,我是张三";
+        String answer = loveApp.doChat(message, charId);
+        Assertions.assertNotNull(answer);
+        //第二轮
+        message = "我想让我的另外一半（李四）更加爱我";
+        answer = loveApp.doChat(message, charId);
+        Assertions.assertNotNull(answer);
+        //第三轮
+        message = "我的另外一半叫什么来着？刚刚和你说过，帮我回忆一下";
+        answer = loveApp.doChat(message, charId);
+        Assertions.assertNotNull(answer);
+    }
+}
